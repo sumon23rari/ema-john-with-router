0fa73d5cb03b8616465d986327dab016d8454d9f
sumon23rari <sumon784969@gmail.com>
2022-01-21 21:08:05 +0600

    updated the inventory page

--- src/Inventory/Inventory.js
+++ src/Inventory/Inventory.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Inventory = () => {
     return (
         <div>
-            <h3>this is inventory</h3>
+            <h3>this is inventory page</h3>
         </div>
     );
 };
